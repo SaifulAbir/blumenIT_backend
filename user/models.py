b9317193684d32cb600953152fc6016f9c8dbed0
Saiful Islam <saiful.abir20@gmail.com>
2022-01-04 11:25:26 +0600

    user model created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,69 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', ]
+
+    objects = UserManager()
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        full_name = None
+        if self.first_name or self.last_name:
+            full_name = self.first_name + " " + self.last_name
+        elif self.username:
+            full_name = self.username
+        else:
+            full_name = self.email
+        return full_name
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ['-is_active']
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        db_table = 'users'

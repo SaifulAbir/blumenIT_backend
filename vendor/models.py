cfed29b36bc1018453a10f8acb941042ff6e89aa
Saiful Islam <saiful.abir20@gmail.com>
2022-01-06 13:53:24 +0600

    vendor model created

--- vendor/models.py
+++ vendor/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from ecommerce.models import AbstractTimeStamp
 from django.utils.translation import gettext as _
 
+from user.models import User
+
 
 class VendorRequest(AbstractTimeStamp):
     VENDOR_STATUSES = [
@@ -28,3 +30,25 @@ class VendorRequest(AbstractTimeStamp):
         verbose_name = "Vendor Request"
         verbose_name_plural = "Vendor Requests"
         db_table = 'vendor_requests'
+
+
+class Vendor(AbstractTimeStamp):
+    organization_name = models.CharField(
+        max_length=254, null=False, blank=False, verbose_name=_('Organization/ Vendor Name'))
+    address = models.CharField(
+        max_length=254, null=True, blank=True, verbose_name=_('Address'))
+    vendor_admin = models.ForeignKey(
+        User, on_delete=models.PROTECT, blank=False, null=False, related_name="vendor_admin",
+        verbose_name=_('Vendor Admin'))
+    vendor_request = models.ForeignKey(
+        VendorRequest, on_delete=models.PROTECT, blank=True, null=True, related_name="vendor_request",
+        verbose_name=_('Vendor Request'))
+    phone = models.CharField(max_length=255, null=True, blank=True, default="None")
+
+    def __str__(self):
+        return self.organization_name
+
+    class Meta:
+        verbose_name_plural = "Vendors"
+        verbose_name = "Vendor"
+        db_table = 'vendors'

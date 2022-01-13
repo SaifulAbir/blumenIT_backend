135e50f0f5f95e6595122c1f3e23807488ef59c7
Arnavbd1971 <shivbd1971@gmail.com>
2022-01-13 15:46:02 +0600

    product update done commit

--- product/serializers.py
+++ product/serializers.py
@@ -85,7 +85,13 @@ class ProductSerializer(serializers.ModelSerializer):
         return product_instance
 
 class ProductUpdateSerializer(serializers.ModelSerializer):
-    # product_tags = ProductTagsSerializer(many=True, read_only=True)
+    product_tags = ProductTagsSerializer(many=True, read_only=True)
+    product_colors = ProductColorsSerializer(many=True, read_only=True)
+    product_sizes = ProductSizesSerializer(many=True, read_only=True)
+
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tags.objects.all(), many=True, write_only=True)
+    colors = serializers.PrimaryKeyRelatedField(queryset=Colors.objects.all(), many=True, write_only=True)
+    sizes = serializers.PrimaryKeyRelatedField(queryset=Sizes.objects.all(), many=True, write_only=True)
 
     class Meta:
         model = Product
@@ -104,11 +110,33 @@ class ProductUpdateSerializer(serializers.ModelSerializer):
             'is_featured',
             'product_category',
             'product_brand',
-            # 'tags',
-            # 'product_tags'
+            'tags',
+            'product_tags',
+            'colors',
+            'product_colors',
+            'sizes',
+            'product_sizes'
         ]
 
     def update(self, instance, validated_data):
+        tags = validated_data.pop('tags')
+        colors = validated_data.pop('colors')
+        sizes = validated_data.pop('sizes')
+        if tags:
+            ProductTags.objects.filter(product=instance).delete()
+            for tag in tags:
+                ProductTags.objects.create(name=tag, product=instance)
+
+        if colors:
+            ProductColors.objects.filter(product=instance).delete()
+            for color in colors:
+                ProductColors.objects.create(name=color, product=instance)
+
+        if sizes:
+            ProductSizes.objects.filter(product=instance).delete()
+            for size in sizes:
+                ProductSizes.objects.create(name=sizes, product=instance)
+
         validated_data.update({"modified_by": self.context['request'].user.id, "modified_at": timezone.now()})
         return super().update(instance, validated_data)
 

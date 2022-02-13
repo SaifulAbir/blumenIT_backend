5d80b407113658e8bcdca7341f073adceca70def
Arnavbd1971 <shivbd1971@gmail.com>
2022-02-13 15:11:11 +0600

    vendor add

--- product/models.py
+++ product/models.py
@@ -95,7 +95,7 @@ class Product(AbstractTimeStamp):
     created_by = models.CharField(max_length=255, null=True)
     thumbnail = models.FileField(upload_to='products', blank=True, null=True)
     cover = models.FileField(upload_to='products', blank=True, null=True)
-    vendor = models.ForeignKey(Vendor, related_name='vendor', blank=True, null=True, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, related_name='vendor', blank=False, null=False, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Product'

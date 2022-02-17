55cd03d1190daac2dabe6242f08800160347660b
Saiful Islam <saiful.abir20@gmail.com>
2022-02-17 15:13:03 +0600

    vendor request list api done

--- vendor/api/views.py
+++ vendor/api/views.py
@@ -1,6 +1,6 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import AllowAny
-
+from vendor.models import VendorRequest
 from vendor.serializers import VendorRequestSerializer
 
 
@@ -10,3 +10,8 @@ class VendorRequestAPIView(CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         return super(VendorRequestAPIView, self).post(request, *args, **kwargs)
+
+
+class VendorRequestListAPI(ListAPIView):
+    queryset = VendorRequest.objects.all()
+    serializer_class = VendorRequestSerializer

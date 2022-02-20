7edb06c88faac1591d957ecde7171013b8aacb6c
Saiful Islam <saiful.abir20@gmail.com>
2022-02-20 13:04:35 +0600

    subscription api done

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from user.api.views import RegisterUser, LoginUser, VerifyUserAPIView, CustomerRetrieveUpdateAPIView
+from user.api.views import RegisterUser, LoginUser, VerifyUserAPIView, CustomerRetrieveUpdateAPIView, \
+    SubscriptionAPIView
 
 urlpatterns = [
     path('login/', LoginUser.as_view({'post': 'create'}), name='login_user'),
     path('register-user/', csrf_exempt(RegisterUser.as_view({'post': 'create'})), name='register_user'),
     path('verify-user/<str:verification_token>', VerifyUserAPIView.as_view()),
     path('customer-profile/', CustomerRetrieveUpdateAPIView.as_view()),
+    path('subscription/', SubscriptionAPIView.as_view()),
 ]

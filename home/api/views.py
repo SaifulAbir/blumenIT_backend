07871959f78cda2210dbab53bdcafedf13d9a754
Saiful Islam <saiful.abir20@gmail.com>
2022-02-24 10:23:20 +0600

    recent view api modified

--- home/api/views.py
+++ home/api/views.py
@@ -63,5 +63,6 @@ class RecentAPIView(APIView):
     def get(self, request):
         user = self.request.user.id
         recent_view = Product.objects.filter(id__in = ProductView.objects.filter(user=user).values('product').order_by('-view_count'))[:24]
+        # recent_view = Product.objects.filter(product_view_count__view_date__gt=)[:24]
         recent_view_serializer = productListSerializer(recent_view, many=True)
         return Response({"recent_view":recent_view_serializer.data })

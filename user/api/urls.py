78c2a725dc9b5488ad7a596ad35b2c4471860b35
Saiful Islam <saiful.abir20@gmail.com>
2022-02-27 10:45:48 +0600

    customer profile update api url added

--- user/api/urls.py
+++ user/api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register-user/', csrf_exempt(RegisterUser.as_view({'post': 'create'})), name='register_user'),
     path('verify-user/<str:verification_token>', VerifyUserAPIView.as_view()),
     path('customer-profile/', CustomerRetrieveUpdateAPIView.as_view()),
+    path('customer-profile/update/', CustomerRetrieveUpdateAPIView.as_view()),
     path('subscription/', SubscriptionAPIView.as_view()),
     path('change-password/', change_password),
 ]

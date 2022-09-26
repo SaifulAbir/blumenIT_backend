230f067ab210c8c454a8853b74d061fcf52c7b78
Arnavbd1971 <shivbd1971@gmail.com>
2022-09-26 10:05:00 +0600

    commit

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cart.models import BillingAddress, PaymentType, ShippingType, Coupon, UseRecordOfCoupon, Order, OrderItem
+from cart.models import BillingAddress, PaymentType, ShippingType, Coupon, UseRecordOfCoupon, Order, VendorOrder, OrderItem
 
 # Register your models here.
 admin.site.register(ShippingType)
@@ -7,11 +7,16 @@ admin.site.register(Coupon)
 admin.site.register(UseRecordOfCoupon)
 admin.site.register(PaymentType)
 admin.site.register(BillingAddress)
+admin.site.register(VendorOrder)
 
 # admin.site.register(Order)
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     fields = ['product', 'quantity']
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [

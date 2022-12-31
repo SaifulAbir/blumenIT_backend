9e84eb78ff42101ac13805123f6a7914a7706aa6
Arnavbd1971 <shivbd1971@gmail.com>
2023-01-01 01:06:43 +0600

    commit

--- stuff/api/urls.py
+++ stuff/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from stuff.api.views import StuffListAPI, CreateStuffAPI
+from stuff.api.views import StuffListAPI, CreateStuffAPI, UpdateStuffAPIView, RoleListAPIView
 
 urlpatterns = [
     path('stuff/stuff-list/<int:pagination>/', StuffListAPI.as_view(), name='stuff_list'),
     path('stuff/create-stuff/', CreateStuffAPI.as_view(), name='create_stuff'),
-    # path('stuff/update-stuff/<int:id>/', UpdateStuffAPIView.as_view(), name='update_stuff'),
+    path('stuff/update-stuff/<int:id>/', UpdateStuffAPIView.as_view(), name='update_stuff'),
+    path('stuff/role-list/<int:pagination>/', RoleListAPIView.as_view(), name='role_list'),
+    path('stuff/create-role/<int:pagination>/', RoleListAPIView.as_view(), name='role_list'),
 ]

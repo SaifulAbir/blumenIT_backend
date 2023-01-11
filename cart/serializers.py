98914dfbea29cea6378a17f2345b64ef5e39b00c
Arnavbd1971 <shivbd1971@gmail.com>
2023-01-11 11:21:30 +0600

    sub total issue problem solved

--- cart/serializers.py
+++ cart/serializers.py
@@ -94,6 +94,8 @@ class CheckoutDetailsSerializer(serializers.ModelSerializer):
         prices = []
         for order_item in order_items:
             price = order_item.unit_price
+            if order_item.unit_price_after_add_warranty != 0.0:
+                price = order_item.unit_price_after_add_warranty
             quantity = order_item.quantity
             t_price = float(price) * float(quantity)
             prices.append(t_price)
@@ -115,6 +117,8 @@ class CheckoutDetailsSerializer(serializers.ModelSerializer):
         total_price = 0
         for order_item in order_items:
             price = order_item.unit_price
+            if order_item.unit_price_after_add_warranty != 0.0:
+                price = order_item.unit_price_after_add_warranty
             quantity = order_item.quantity
             t_price = float(price) * float(quantity)
             prices.append(t_price)

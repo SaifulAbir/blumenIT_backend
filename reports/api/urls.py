bdf05a22202084a1fcc2e3560aaf21e731a4fdd0
Arnavbd1971 <shivbd1971@gmail.com>
2023-01-15 12:14:21 +0600

    commit

--- reports/api/urls.py
+++ reports/api/urls.py
@@ -4,22 +4,22 @@ from reports.api.views import SalesReportAPI, SalesReportSearchAPI, VendorProduc
     ProductWishlistReportAPI, ProductWishlistReportSearchAPI
 
 urlpatterns = [
-    path('reports/sales-report/<int:pagination>/', SalesReportAPI.as_view()),
-    path('reports/sales-report/search/<int:pagination>/', SalesReportSearchAPI.as_view()),
+    path('admin/reports/sales-report/<int:pagination>/', SalesReportAPI.as_view()),
+    path('admin/reports/sales-report/search/<int:pagination>/', SalesReportSearchAPI.as_view()),
 
-    path('reports/vendor-product-report/<int:pagination>/', VendorProductReportAPI.as_view()),
-    path('reports/vendor-product-report/search/<int:pagination>/', VendorProductReportSearchAPI.as_view()),
+    path('admin/reports/vendor-product-report/<int:pagination>/', VendorProductReportAPI.as_view()),
+    path('admin/reports/vendor-product-report/search/<int:pagination>/', VendorProductReportSearchAPI.as_view()),
 
-    path('reports/in-house-product/<int:pagination>/', InHouseProductReportAPI.as_view()),
-    path('reports/in-house-product/search/<int:pagination>/', InHouseProductReportSearchAPI.as_view()),
+    path('admin/reports/in-house-product/<int:pagination>/', InHouseProductReportAPI.as_view()),
+    path('admin/reports/in-house-product/search/<int:pagination>/', InHouseProductReportSearchAPI.as_view()),
 
-    path('reports/seller-products-sale/<int:pagination>/', SellerProductsSaleReportAPI.as_view()),
-    path('reports/seller-products-sale/search/<int:pagination>/', SellerProductsSaleReportSearchAPI.as_view()),
+    path('admin/reports/seller-products-sale/<int:pagination>/', SellerProductsSaleReportAPI.as_view()),
+    path('admin/reports/seller-products-sale/search/<int:pagination>/', SellerProductsSaleReportSearchAPI.as_view()),
 
-    path('reports/product-stock/<int:pagination>/', ProductStockReportAPI.as_view()),
-    path('reports/product-stock/search/<int:pagination>/', ProductStockReportSearchAPI.as_view()),
+    path('admin/reports/product-stock/<int:pagination>/', ProductStockReportAPI.as_view()),
+    path('admin/reports/product-stock/search/<int:pagination>/', ProductStockReportSearchAPI.as_view()),
 
-    path('reports/product-wishlist/<int:pagination>/', ProductWishlistReportAPI.as_view()),
-    path('reports/product-wishlist/search/<int:pagination>/', ProductWishlistReportSearchAPI.as_view()),
+    path('admin/reports/product-wishlist/<int:pagination>/', ProductWishlistReportAPI.as_view()),
+    path('admin/reports/product-wishlist/search/<int:pagination>/', ProductWishlistReportSearchAPI.as_view()),
 
 ]

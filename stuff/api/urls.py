9d9bd6084e1cabde2593b432f159d96d00c19723
Arnavbd1971 <shivbd1971@gmail.com>
2023-01-17 12:48:00 +0600

    commit

--- stuff/api/urls.py
+++ stuff/api/urls.py
@@ -3,14 +3,14 @@ from stuff.api.views import StuffListAPI, CreateStuffAPI, UpdateStuffAPIView, Ro
     AdminRoleUpdateAPIView, PermissionListAPIView, AdminPermissionCreateAPIView, AdminPermissionUpdateAPIView
 
 urlpatterns = [
-    path('admin/stuff/stuff-list/<int:pagination>/', StuffListAPI.as_view(), name='stuff_list'),
-    path('admin/stuff/create-stuff/', CreateStuffAPI.as_view(), name='create_stuff'),
-    path('admin/stuff/update-stuff/<int:id>/', UpdateStuffAPIView.as_view(), name='update_stuff'),
-    path('admin/stuff/role-list/<int:pagination>/', RoleListAPIView.as_view(), name='role_list'),
-    path('admin/stuff/create-role/', AdminRoleCreateAPIView.as_view(), name='create_role'),
-    path('admin/stuff/update-role/<int:id>/', AdminRoleUpdateAPIView.as_view(), name='update_role'),
+    path('admin/staff/staff-list/<int:pagination>/', StuffListAPI.as_view(), name='stuff_list'),
+    path('admin/staff/create-staff/', CreateStuffAPI.as_view(), name='create_stuff'),
+    path('admin/staff/update-staff/<int:id>/', UpdateStuffAPIView.as_view(), name='update_stuff'),
+    path('admin/staff/role-list/<int:pagination>/', RoleListAPIView.as_view(), name='role_list'),
+    path('admin/staff/create-role/', AdminRoleCreateAPIView.as_view(), name='create_role'),
+    path('admin/staff/update-role/<int:id>/', AdminRoleUpdateAPIView.as_view(), name='update_role'),
 
-    path('admin/stuff/permission-list/<int:pagination>/', PermissionListAPIView.as_view(), name='permission_list'),
-    path('admin/stuff/create-permission/', AdminPermissionCreateAPIView.as_view(), name='create_permission'),
-    path('admin/stuff/update-permission/<int:id>/', AdminPermissionUpdateAPIView.as_view(), name='update_permission'),
+    path('admin/staff/permission-list/<int:pagination>/', PermissionListAPIView.as_view(), name='permission_list'),
+    path('admin/staff/create-permission/', AdminPermissionCreateAPIView.as_view(), name='create_permission'),
+    path('admin/staff/update-permission/<int:id>/', AdminPermissionUpdateAPIView.as_view(), name='update_permission'),
 ]

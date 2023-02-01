6f2321756d3dc599d231b030de5a0976227c8399
Arnavbd1971 <shivbd1971@gmail.com>
2023-02-01 11:28:26 +0600

    commit

--- vendor/api/views.py
+++ vendor/api/views.py
@@ -172,7 +172,7 @@ class AdminProductListAPI(ListAPIView):
             request = self.request
             type = request.GET.get('type')
 
-            queryset = Product.objects.filter(is_active=False).order_by('-created_at')
+            queryset = Product.objects.filter(is_active=True).order_by('-created_at')
 
             if type == 'digital':
                 queryset = queryset.filter(digital=True)

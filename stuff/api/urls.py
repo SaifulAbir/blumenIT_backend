c2519a84d1701a2bf2d11038800bcfbea617d7ae
Arnavbd1971 <shivbd1971@gmail.com>
2023-02-22 11:33:29 +0600

    staff urls update

--- stuff/api/urls.py
+++ stuff/api/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('admin/staff/create-staff/', CreateStuffAPI.as_view(), name='create_stuff'),
     path('admin/staff/update-staff/<int:id>/', UpdateStuffAPIView.as_view(), name='update_stuff'),
     path('admin/staff/staff-delete/<int:id>/', AdminStuffDeleteAPIView.as_view()),
-    path('admin/staff/role-list/<int:pagination>/', RoleListAPIView.as_view(), name='role_list'),
+    path('admin/staff/role-list/', RoleListAPIView.as_view(), name='role_list'),
     path('admin/staff/create-role/', AdminRoleCreateAPIView.as_view(), name='create_role'),
     path('admin/staff/update-role/<int:id>/', AdminRoleUpdateAPIView.as_view(), name='update_role'),
 
-    path('admin/staff/permission-list/<int:pagination>/', PermissionListAPIView.as_view(), name='permission_list'),
+    path('admin/staff/permission-list/', PermissionListAPIView.as_view(), name='permission_list'),
     path('admin/staff/create-permission/', AdminPermissionCreateAPIView.as_view(), name='create_permission'),
     path('admin/staff/update-permission/<int:id>/', AdminPermissionUpdateAPIView.as_view(), name='update_permission'),
 ]

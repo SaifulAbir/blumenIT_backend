8344bd02b8a0fc8df90abfb4a6056439fa918a66
jubayedulislam <arnok2084@gmail.com>
2023-02-23 09:45:45 +0600

    subcategory_list

--- vendor/serializers.py
+++ vendor/serializers.py
@@ -200,7 +200,7 @@ class UpdateCategorySerializer(serializers.ModelSerializer):
 class AdminSubCategoryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
-        fields = ['id', 'title', 'ordering_number', 'category', 'is_featured']
+        fields = ['id', 'title', 'ordering_number', 'category', 'is_featured', 'pc_builder']
 
 
 class AddNewSubCategorySerializer(serializers.ModelSerializer):

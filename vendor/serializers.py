145de30ecaebb72a1e96605eccac830ecbf6091a
Saiful Islam <saiful.abir20@gmail.com>
2023-04-04 11:57:10 +0600

    settings apis are done

--- vendor/serializers.py
+++ vendor/serializers.py
@@ -2601,5 +2601,23 @@ class WebsiteConfigurationViewSerializer(serializers.ModelSerializer):
             return []
 
 
+class GeneralSettingsViewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = HomeSingleRowData
+        fields = [
+            'id',
+            'header_logo',
+            'footer_logo',
+            'footer_description',
+            'facebook',
+            'twitter',
+            'instagram',
+            'whatsapp',
+            'messenger',
+            'linkedin',
+            'youtube',
+        ]
+
 
 

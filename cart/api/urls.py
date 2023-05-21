7a02c53f8bfb8e00da72b3fcfe7af16b5e8c6247
Arnavbd1971 <shivbd1971@gmail.com>
2023-05-21 14:47:23 +0600

    message

--- cart/api/urls.py
+++ cart/api/urls.py
@@ -10,10 +10,12 @@ urlpatterns = [
     path('discount-type-list/', DiscountTypeListAPI.as_view()),
     path('payment-types-list/', PaymentMethodsAPIView.as_view()),
     path('create-delivery-address/', DeliveryAddressCreateAPIView.as_view()),
-    path('update-delivery-address/<int:id>/', DeliveryAddressUpdateAPIView.as_view()),
+    path('update-delivery-address/<int:id>/',
+         DeliveryAddressUpdateAPIView.as_view()),
     path('delivery-address-list/', DeliveryAddressListAPIView.as_view()),
-    path('delete-delivery-address/<int:id>/', DeliveryAddressDeleteAPIView.as_view()),
-    path('wishlist-add-remove/<int:product_id>/', WishlistAddRemoveAPIView.as_view()),
+    path('delete-delivery-address/<int:id>/',
+         DeliveryAddressDeleteAPIView.as_view()),
+    path('wishlist-add-remove/<int:product_id>/',
+         WishlistAddRemoveAPIView.as_view()),
+    path('shipping-country-list/', ShippingCountryListAPIView.as_view()),
 ]
-
-
